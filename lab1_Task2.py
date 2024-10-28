ac97361dbc3d0abacccd80c3aabae2f944233145
StasYaremchuk <jaremchuk.stanislav1@gmail.com>
2024-10-28 19:45:36 +0200

    Add files via upload

---
+++ lab1_Task2.py
@@ -0,0 +1,11 @@
+holiday = "Christmas"
+print("My favorite holiday is", holiday)
+
+is_adult = False
+is_teen = True
+print("\nadult:", is_adult, "\nteen:", is_teen, "\n")
+
+is_windy = True
+is_calm = True
+result = is_windy or is_calm
+print(result)
